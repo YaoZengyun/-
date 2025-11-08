867913563762f6e8b56a64d7d5e0b5708d811118
QMqaq <2430732194@qq.com>
2025-11-09 00:33:03 +0800

    chore: remove buildozer artifacts from index

--- android_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/arm64-v8a__ndk_target_21/sqlite3/obj/local/arm64-v8a/objs-debug/sqlite3/sqlite3.o.d
+++
@@ -1,2 +0,0 @@
-/home/qmqaq/Anan-s-Sketchbook-Chat-Box-main/android_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/arm64-v8a__ndk_target_21/sqlite3/obj/local/arm64-v8a/objs-debug/sqlite3/sqlite3.o: \
-  /home/qmqaq/Anan-s-Sketchbook-Chat-Box-main/android_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/other_builds/sqlite3/arm64-v8a__ndk_target_21/sqlite3/jni/../sqlite3.c
